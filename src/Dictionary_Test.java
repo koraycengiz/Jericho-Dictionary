b9527dfb3edde4bc95a0c4304810122bd63f1ada
koraycengiz <76446984+koraycengiz@users.noreply.github.com>
2023-03-17 11:41:06 +0300

    Initial commit

---
+++ src/Dictionary_Test.java
@@ -0,0 +1,12 @@
+public class Dictionary_Test {
+
+    public static void hello(){
+        System.out.println("****************************************************************");
+        System.out.println("Welcome to the Jericho Dictionary application\nThis is the first trial of using GitHub in this project :)");
+    }
+
+    public static void main(String[] args) {
+
+        hello();
+    }
+}
