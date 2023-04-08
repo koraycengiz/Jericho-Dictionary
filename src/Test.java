b1b5015bb81b1782fb9d4a98c969bef8158ac5cf
bigloppa <egedenizyasar40@hotmail.com>
2023-04-08 14:36:27 +0300

    .

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
-        Word word = new Word("Büyük","tur");
+        Word word = new Word("yaş","tur");
 
         dictionary.getTranslations(word);
     }
