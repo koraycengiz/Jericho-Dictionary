70971879e9514518daf42ec113cf67cd88d95114
bigloppa <egedenizyasar40@hotmail.com>
2023-04-09 12:52:29 +0300

    changed the translate function for punctiations and modern greek dictionary

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -2,32 +2,44 @@
 
 public class Dictionary {
 
-    final String[] languageList = {"eng","deu","tur","fra","ell","swe","ita"};
+    final String[] languageList = {"eng", "deu", "tur", "fra", "ell", "swe", "ita"};
 
 
-    public void getTranslations(Word word){
+    public void getTranslations(Word word) {
         FileManager fileManager = new FileManager();
         word.setText(word.getText().toLowerCase());
-        for (String language: languageList){
+        for (String language : languageList) {
 
             if (!language.equals(word.getLanguage())) {
-                System.out.println(language+": "+fileManager.searchWord(word, language));
+                System.out.println(language + ": " + translate(fileManager.searchWord(word, language)));
             }
         }
 
     }
 
-    public static String translate(String definition){
+    public static String translate(String definition) {
         String[] lines = definition.split("\n");
+
         String line = lines[1];
-        String[] parts = line.split(" ");
-        if (parts[0].equals("1.")){
-            return parts[1].replaceAll(",","");
-        }else {
-            return parts[0].replaceAll(",","");
+        if (line.isEmpty()){
+            line = lines[2];
         }
+        String[] parts = line.split(" ");
+
+        for (String part : parts) {
+            boolean flag = true;
+            String[] punctiations = {".", "[", "]", ">", "<", "/"};
+
+            for (String punctiation : punctiations) {
+                if (part.contains(punctiation)) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                return part.replaceAll(",","");
+            }
+        }
+        return "Translation not found";
     }
-
-
 
 }
