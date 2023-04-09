e028dc269ab916807d5ac93a0d7bb309eecd1dc2
hilalsinem <hilalsinemsayar@gmail.com>
2023-04-09 16:54:36 +0300

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -15,6 +15,7 @@ public class GUI extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -30,36 +31,63 @@ public class GUI extends Application {
             public void handle(ActionEvent t) {
                 GridPane gp = new GridPane();
                 Stage stage = new Stage();
-                Scene scene = new Scene(gp);
                 stage.setTitle("contact");
                 VBox vbox = new VBox();
+                VBox.setVgrow(gp, Priority.ALWAYS);
+                Scene scene = new Scene(vbox);
 
-                Label lblTitle = new Label("Contact Us");
+                Label Title = new Label("Contact Us");
 
-                Label lblName = new Label("Name");
-                TextField tfName = new TextField();
+                Label Name = new Label("Name");
+                Label Surname = new Label("Surname");
+                Label Email = new Label("Email");
+                Label Selection = new Label("Receive reply via ");
+                Label Problem = new Label("Problem");
+                Label Description = new Label("Description");
 
-                Label lblSurname = new Label("Surname");
-                TextField tfSurname = new TextField();
+                TextField Nametf = new TextField();
+                TextField Surnametf = new TextField();
+                TextField Emailtf = new TextField();
+                TextField Problemtf = new TextField();
+                TextArea Descriptionta = new TextArea();
 
-                Label lblEmail = new Label("Email");
-                TextField tfEmail = new TextField();
+                ObservableList<String> options = FXCollections.observableArrayList("Email", "Messages", "Phone Call");
+                ComboBox<String> Selectioncb = new ComboBox<>(options);
 
-                Label lblSelection = new Label("Reply Me");
-                ObservableList<String> priorities = FXCollections.observableArrayList("via Email", "via Messages", "via Phone Call");
-                ComboBox<String> cbSelection = new ComboBox<>(priorities);
+                gp.add(Title, 1, 1);
+                gp.add(Name, 0, 2);
+                gp.add(Nametf, 1, 2);
+                gp.add(Surname, 0, 3);
+                gp.add(Surnametf, 1, 3);
+                gp.add(Email, 0,4);
+                gp.add(Emailtf, 1,4);
+                gp.add(Problem, 0, 5);
+                gp.add(Problemtf, 1, 5);
+                gp.add(Description, 0, 6);
+                gp.add(Descriptionta, 1, 6);
+                gp.add(Selection, 0,7);
+                gp.add(Selectioncb, 1,7);
 
-                Label lblProblem = new Label("Problem");
-                TextField tfProblem = new TextField();
+                Separator seperator = new Separator();
 
-                Label lblDescription = new Label("Description");
-                TextArea taDescription = new TextArea();
+                ButtonBar buttons = new ButtonBar();
 
-                gp.add( lblTitle,       1, 1);  // empty item at 0,0
-                gp.add( lblName,       0, 2); gp.add(tfName,        1, 2);
-                gp.add( lblSurname,    0, 3); gp.add( tfSurname,    1, 3);
-                gp.add( lblProblem,     0, 4); gp.add( tfProblem,     1, 4);
-                gp.add( lblDescription, 0, 5); gp.add( taDescription, 1, 5);
+                Button sendButton = new Button("Send");
+                Button closeButton = new Button("Close");
+
+                buttons.getButtons().addAll(sendButton, closeButton);
+
+                closeButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("closed"); // to test
+                        stage.close();
+                    }
+                }); {
+
+                }
+
+                vbox.getChildren().addAll(gp, seperator, buttons);
 
                 stage.setScene(scene);
                 stage.show();
@@ -67,7 +95,7 @@ public class GUI extends Application {
             }
         });
 
-        menu.getItems().addAll(about,support);
+        menu.getItems().addAll(about, support);
         menuBar.getMenus().add(menu);
         root.setTop(menuBar);
 
@@ -86,43 +114,41 @@ public class GUI extends Application {
         choiceBox.getItems().add("Italian");
         choiceBox.getItems().add("Swedish");
 
-        HBox.setHgrow(label,Priority.ALWAYS);
-        HBox.setHgrow(choiceBox,Priority.ALWAYS);
+        HBox.setHgrow(label, Priority.ALWAYS);
+        HBox.setHgrow(choiceBox, Priority.ALWAYS);
 
         HBox secondLine = new HBox(8);
         secondLine.setAlignment(Pos.CENTER);
 
-        VBox.setMargin(firstLine,new Insets(8));
+        VBox.setMargin(firstLine, new Insets(8));
 
-        firstLine.getChildren().addAll(label,choiceBox);
+        firstLine.getChildren().addAll(label, choiceBox);
 
         Label word = new Label("Word:");
         TextField textField = new TextField();
         Button benjamin = new Button("Search");
 
-        HBox.setHgrow(word,Priority.ALWAYS);
-        HBox.setHgrow(textField,Priority.ALWAYS);
-        HBox.setHgrow(benjamin,Priority.ALWAYS);
+        HBox.setHgrow(word, Priority.ALWAYS);
+        HBox.setHgrow(textField, Priority.ALWAYS);
+        HBox.setHgrow(benjamin, Priority.ALWAYS);
 
-        VBox.setMargin(secondLine,new Insets(8));
-        secondLine.getChildren().addAll(word,textField,benjamin);
+        VBox.setMargin(secondLine, new Insets(8));
+        secondLine.getChildren().addAll(word, textField, benjamin);
 
         HBox thirdLine = new HBox(8);
         thirdLine.setAlignment(Pos.CENTER);
 
         ListView listview = new ListView();
 
-        HBox.setHgrow(listview,Priority.ALWAYS);
+        HBox.setHgrow(listview, Priority.ALWAYS);
 
-        VBox.setMargin(thirdLine,new Insets(8));
+        VBox.setMargin(thirdLine, new Insets(8));
         thirdLine.getChildren().addAll(listview);
 
 
+        mainLayout.getChildren().addAll(root, firstLine, secondLine, thirdLine);
 
-
-        mainLayout.getChildren().addAll(root,firstLine,secondLine,thirdLine);
-
-        BackgroundFill backgroundFill = new BackgroundFill(Color.PINK, CornerRadii.EMPTY,Insets.EMPTY);
+        BackgroundFill backgroundFill = new BackgroundFill(Color.PINK, CornerRadii.EMPTY, Insets.EMPTY);
         Background background = new Background(backgroundFill);
         mainLayout.setBackground(background);
 
@@ -131,8 +157,7 @@ public class GUI extends Application {
         stage.setTitle("Dictionary Jericho");
         stage.setScene(scene);
         stage.show();
-    }
 }
 
- 
 
+}
